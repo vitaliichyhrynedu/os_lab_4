08d1c0a4aebaf2987fa1919d93b5f6b7d3248ac8
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-12-05 19:40:48 +0200

    hardware/storage: implement a model for a blocked storage device

---
+++ src/hardware/storage.rs
@@ -0,0 +1,72 @@
+pub const BLOCK_SIZE: u64 = 4096;
+
+/// A model for a physical storage device
+pub struct Storage {
+    blocks: Box<[Block]>,
+}
+
+impl Storage {
+    /// Constructs a zero-initialized `Storage` of specified size and block size
+    pub fn new(size: u64) -> Self {
+        let block_count = size.div_ceil(BLOCK_SIZE);
+        let blocks = vec![Block::new(); block_count as usize].into_boxed_slice();
+        Self { blocks }
+    }
+
+    /// Returns the number of blocks
+    pub fn block_count(&self) -> u64 {
+        self.blocks.len() as u64
+    }
+
+    /// Reads data from the block at specified index into `dst` block
+    pub fn read_block(&self, index: u64, dst: &mut Block) -> Result<(), Error> {
+        let src = &self
+            .blocks
+            .get(index as usize)
+            .ok_or(Error::BlockIndexOutOfBounds)?;
+        *dst = **src;
+        Ok(())
+    }
+
+    /// Writes data from the `src` block into the block at specified index
+    pub fn write_block(&mut self, index: u64, src: &Block) -> Result<(), Error> {
+        let dst = &mut self
+            .blocks
+            .get_mut(index as usize)
+            .ok_or(Error::BlockIndexOutOfBounds)?;
+        **dst = *src;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    BlockIndexOutOfBounds,
+}
+
+/// A model for a block
+#[derive(Clone, Copy)]
+pub struct Block {
+    data: [u8; BLOCK_SIZE as usize],
+}
+
+impl Block {
+    /// Constructs a zero-initialized `Block` of specified size
+    pub fn new() -> Self {
+        Self {
+            data: [0u8; BLOCK_SIZE as usize],
+        }
+    }
+}
+
+impl From<&[u8]> for Block {
+    /// Creates a block from a slice of bytes
+    ///
+    /// Panics if the slice is too large, pads with zeros if too small
+    fn from(bytes: &[u8]) -> Self {
+        let mut block = Self::new();
+        let length = bytes.len().min(BLOCK_SIZE as usize);
+        block.data[0..length].copy_from_slice(&bytes[..length]);
+        block
+    }
+}
